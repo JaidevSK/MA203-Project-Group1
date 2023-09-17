c6541ffeb609521617678f274f4257242327b8a4
JaidevSK <134712750+JaidevSK@users.noreply.github.com>
2023-09-17 16:52:31 +0530

    Added LU Decomposition

--- ProjectMA203.py
+++ ProjectMA203.py
@@ -14,6 +14,39 @@ import seaborn as sns
 import plotly.express as ex
 import plotly.graph_objects as go
 
+
+def lu_decomposition(matrix):
+    n = len(matrix)
+    lower = np.zeros((n, n))
+    upper = np.zeros((n, n))
+
+    for i in range(n):
+        # Upper Triangular
+        for k in range(i, n):
+            sum = 0
+            for j in range(i):
+                sum += (lower[i][j] * upper[j][k])
+            upper[i][k] = matrix[i][k] - sum
+
+        # Lower Triangular
+        for k in range(i, n):
+            if i == k:
+                lower[i][i] = 1
+            else:
+                sum = 0
+                for j in range(i):
+                    sum += (lower[k][j] * upper[j][i])
+                lower[k][i] = (matrix[k][i] - sum) / upper[i][i]
+
+    return lower, upper
+
+# Example usage
+matrix = np.array([[1, -2, -2, -3], [3, -9, 0, -9], [-1, 2, 4, 7], [-3, -6, 26, 2]])
+lower, upper = lu_decomposition(matrix)
+
+
+
+
 st.header("MA 203 Project Group 1")
 st.title("Modelling the heating in an IC with multiple heat transfer mechanisms using Numerical Methods")
 st.sidebar.title("User Inputs")
